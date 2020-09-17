d9c5b4b78ba607820b9659c60091d7b77061218d
takahashi0212 <rt6965@yahoo.co.jp>
2020-09-17 16:44:25 +0900

    特定のIDのみを表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   def index
+    @posts = Post.all.order("created_at DESC")
   end
   def new
     @post = Post.new
@@ -13,6 +14,26 @@ class PostsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def chair
+    @posts = Post.all.order("created_at DESC")
+    @posts = Post.where(category_id: 4)
+  end
+   
+  def table
+    @posts = Post.all.order("created_at DESC")
+    @posts = Post.where(category_id: 2)
+  end
+
+  def grill
+    @posts = Post.all.order("created_at DESC")
+    @posts = Post.where(category_id: 3)
+  end
+
+  def tent
+    @posts = Post.all.order("created_at DESC")
+    @posts = Post.where(category_id: 5)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   get 'posts/index'
   root to: "posts#index"
   resources :posts, only: [:new, :create]
+
+  ## table.htmlを追加
+  get "posts/chair" => "posts#chair"
+  get "posts/table" => "posts#table"
+  get "posts/grill" => "posts#grill"
+  get "posts/tent"  => "posts#tent"
 end
